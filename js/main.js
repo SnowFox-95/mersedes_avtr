233b5b9d883b137f82fcd710ce91504bfb1cdee1
SnowFox-95 <321mts95@gmail.com>
2022-11-01 15:57:26 +0300

    slider on slick + css

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('.design-slider').slick({
+        dots: false,
+        slidesToShow: 4,
+        variableWidth: true,
+        prevArrow: '<img class="arrow arrow-left" src="images/Arrow_left.svg" alt="">',
+        nextArrow: '<img class="arrow arrow-right" src="images/Arrow_right.svg" alt="">',
+    });
+});
